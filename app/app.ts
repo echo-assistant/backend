bd2ecf9eed0c5526cb9008a229fef104ba13fe6c
Vedran Blazenka <blazenka.vedran@gmail.com>
2019-06-12 21:19:56 +0200

    general: Setup basic GraphQL server

--- app/app.ts
+++ app/app.ts
@@ -1,19 +1,28 @@
-import * as express from 'express';
-import * as bodyParser from 'body-parser';
-import {RequestHandlerParams} from "express-serve-static-core";
+import * as express from "express";
+import * as bodyParser from "body-parser";
+import { RequestHandlerParams } from "express-serve-static-core";
+
+import { ApolloServer, gql } from "apollo-server-express";
+
+import schema from "./schema";
 
 class App {
-    public app: express.Application;
+  public app: express.Application;
+  public server: ApolloServer;
 
-    constructor() {
-        this.app = express();
-        this.runConfig();
-    }
+  constructor() {
+    this.app = express();
+    this.server = new ApolloServer(schema);
+    this.runConfig();
+  }
 
-    runConfig(): void {
-       this.app.use(bodyParser.urlencoded({ extended: true }));
-       this.app.use(bodyParser.json());
-    }
+  runConfig(): void {
+    this.app.use(bodyParser.urlencoded({ extended: true }));
+    this.app.use(bodyParser.json());
+    this.server.applyMiddleware({ app: this.app });
+
+    console.log(`GraphQL running on ${this.server.graphqlPath}`);
+  }
 }
 
 export default new App().app;
